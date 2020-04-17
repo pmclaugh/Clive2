9f47fd6c394d584bb7876b6bb2007f8355b6bbb3
Phil McLaughlin <phil@Phils-MacBook-Pro.local>
2020-04-17 01:32:11 -0600

    just rearranging main

--- src/main.py
+++ src/main.py
@@ -12,7 +12,6 @@ from load import load_obj
 WINDOW_WIDTH = 200
 WINDOW_HEIGHT = 200
 
-
 @timed
 def render_something():
     s = datetime.now()
@@ -21,11 +20,12 @@ def render_something():
     box = Box(point(-5, -1, -5), point(5, 9, 5))
     bvh = BoundingVolumeHierarchy(load_obj('../resources/teapot.obj') + triangles_for_box(box))
     print(datetime.now() - s, 'loading/compiling')
-    try:
-        parallel_capture(c, bvh.root.box)
-    except KeyboardInterrupt:
-        pass
-    return tone_map(c)
+    parallel_capture(c, bvh.root.box)
+
+
+if __name__ == '__main__':
+    cv2.imshow('render', render_something())
+    cv2.waitKey(0)
 
 # performance test 200x200 10 samples
 # parallel_pixel_capture - 33.9686
@@ -43,9 +43,3 @@ def render_something():
 #  - jit OBJ loading and bvh construction, eliminate TreeBox class
 
 # todo: Known Bugs
-#  - Adding the teapot to the box scene causes all collision to fail if camera is inside box
-
-
-if __name__ == '__main__':
-    cv2.imshow('render', render_something())
-    cv2.waitKey(0)
