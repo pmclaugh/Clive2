6c29b56131410bb03c3dce84be7d6017175849d3
Phil McLaughlin <phil@Phils-MacBook-Pro.local>
2020-11-19 22:10:02 -0700

    cleaning up main

---
+++ src/config.py
@@ -0,0 +1,35 @@
+from bvh import triangles_for_box
+from collision import simple_collision_screen_sample
+from bidirectional import bidirectional_screen_sample
+from unidirectional import unidirectional_screen_sample
+from primitives import point, FastBox
+import pickle
+from fastbvh import fastBVH
+from camera import composite_image, tone_map
+
+default_config = {
+    'cam_center': point(0, 2, 6),
+    'cam_direction': point(0, 0, -1),
+    'window_width': 160,
+    'window_height': 90,
+    'sample_count': 20,
+    'primitives': triangles_for_box(FastBox(point(-10, -3, -10), point(10, 17, 10))),
+    'bvh_constructor': fastBVH,
+    'sample_function': unidirectional_screen_sample,
+    'postprocess_function': lambda x: tone_map(x.image),
+}
+
+bidirectional_config = {
+    'sample_function': bidirectional_screen_sample,
+    'postprocess_function': composite_image,
+}
+
+collision_test_config = {
+    'sample_count': 10,
+    'sample_function': simple_collision_screen_sample,
+    'postprocess_function': lambda x: tone_map(x.image),
+}
+
+teapot_config = {
+    'primitives': teapot_scene,
+}
--- src/main.py
+++ src/main.py
@@ -1,62 +1,21 @@
 import cv2
-from camera import Camera, composite_image, tone_map
+from camera import Camera
 from primitives import point, FastBox
 from datetime import datetime
-from bvh import BoundingVolumeHierarchy, triangles_for_box
+from bvh import triangles_for_box
 from load import load_obj
-from constants import Material
-from collision import simple_collision_screen_sample
-from bidirectional import bidirectional_screen_sample
-from unidirectional import unidirectional_screen_sample
 from collections import ChainMap
-import pickle
-from fastbvh import fastBVH
+from config import default_config, bidirectional_config, teapot_config
 
 WINDOW_WIDTH = 160
 WINDOW_HEIGHT = 90
 SAMPLE_COUNT = 40
 
 teapot_scene = load_obj('../resources/teapot.obj')
+
+# build simple room
 for triangle in triangles_for_box(FastBox(point(-10, -3, -10), point(10, 17, 10))):
     teapot_scene.append(triangle)
-
-default_config = {
-    'cam_center': point(0, 2, 6),
-    'cam_direction': point(0, 0, -1),
-    'window_width': 160,
-    'window_height': 90,
-    'sample_count': 20,
-    'primitives': triangles_for_box(FastBox(point(-10, -3, -10), point(10, 17, 10))),
-    'bvh_constructor': fastBVH,
-    'sample_function': unidirectional_screen_sample,
-    'postprocess_function': lambda x: tone_map(x.image),
-}
-
-bidirectional_config = {
-    'sample_function': bidirectional_screen_sample,
-    'postprocess_function': composite_image,
-}
-
-collision_test_config = {
-    'sample_count': 10,
-    'sample_function': simple_collision_screen_sample,
-    'postprocess_function': lambda x: tone_map(x.image),
-}
-
-teapot_config = {
-    'primitives': teapot_scene,
-}
-
-# todo: branch 'bvh_improvements':
-#  - full implementation of nvidia sbvh - done except for some details
-#    - alpha test for spatial
-#    - reference unsplitting
-#  - bvh caching
-#  - bvh performance test
-#  - unit tests for bvh methods
-#  - jit object splitting
-#  - jit spatial splitting
-
 
 if __name__ == '__main__':
     cfg = ChainMap(bidirectional_config, teapot_config, default_config)
@@ -74,6 +33,17 @@ if __name__ == '__main__':
     else:
         print('done')
     cv2.imwrite('../renders/%s.jpg' % datetime.now(), cfg['postprocess_function'](camera))
+
+
+# todo: branch 'bvh_improvements':
+#  - full implementation of nvidia sbvh - done except for some details
+#    - alpha test for spatial
+#    - reference unsplitting
+#  - bvh caching
+#  - bvh performance test
+#  - unit tests for bvh methods
+#  - jit object splitting
+#  - jit spatial splitting
 
 
 # todo: Feature Schedule
