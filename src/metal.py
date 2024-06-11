simpler, more correct weighting

src/metal.py
@@ -219,7 +219,7 @@ if __name__ == '__main__':
         kernel = f.read()
 
     summed_image = np.zeros((c.pixel_height, c.pixel_width, 3), dtype=np.float32)
-    samples = 20
+    samples = 100
     to_display = np.zeros(summed_image.shape, dtype=np.uint8)
 
     batch_size = c.pixel_width * c.pixel_height
@@ -277,12 +277,15 @@ if __name__ == '__main__':
         print(np.min(retrieved_values))
         print(np.max(retrieved_values))
 
+        camera_paths = np.frombuffer(out_camera_paths, dtype=Path)
+        light_paths = np.frombuffer(out_light_paths, dtype=Path)
+
         summed_image += np.nan_to_num(bidirectional_image)
         if np.any(np.isnan(summed_image)):
             print("NaNs in summed image!!!")
             break
 
-        to_display = tone_map(summed_image)
+        to_display = tone_map(summed_image / (i + 1))
 
         if np.any(np.isnan(to_display)):
             print("NaNs in to_display!!!")